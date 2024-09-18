0a11fd49f72ea929df11910322bd53b979b1d625
Ninja00Shadow <mrshadow813@gmail.com>
2024-09-18 22:47:15 +0200

    combobox create permanent create option

--- src/components/ui/combobox-create.tsx
+++ src/components/ui/combobox-create.tsx
@@ -9,7 +9,6 @@ import {
   Command,
   CommandGroup,
   CommandInput,
-  CommandEmpty,
   CommandList,
 } from "@/components/ui/command"
 import {
@@ -37,127 +36,6 @@ interface ComboboxProps {
   onChange?: (event: string | string[]) => void // Updated to handle multiple selections
   onCreate?: (value: string) => void
 }
-
-// export function ComboboxWithCreate({
-//   options,
-//   selected,
-//   className,
-//   placeholder,
-//   mode = "single",
-//   onChange,
-//   onCreate,
-// }: ComboboxProps) {
-//   const [open, setOpen] = React.useState(false)
-//   const [query, setQuery] = React.useState<string>("")
-//
-//   return (
-//     <div className={cn("block", className)}>
-//       <Popover open={open} onOpenChange={setOpen}>
-//         <PopoverTrigger asChild>
-//           <Button
-//             key={"combobox-trigger"}
-//             variant="outline"
-//             role="combobox"
-//             aria-expanded={open}
-//             className="w-full justify-between"
-//           >
-//             {selected && selected.length > 0 ? (
-//               <div className="relative mr-auto flex flex-grow flex-wrap items-center overflow-hidden">
-//                 <span>
-//                   {mode === "multiple" && Array.isArray(selected)
-//                     ? selected
-//                         .map(
-//                           (selectedValue: string) =>
-//                             options.find((item) => item.value === selectedValue)
-//                               ?.label,
-//                         )
-//                         .join(", ")
-//                     : mode === "single" &&
-//                       options.find((item) => item.value === selected)?.label}
-//                 </span>
-//               </div>
-//             ) : (
-//               (placeholder ?? "Select Item...")
-//             )}
-//             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-//           </Button>
-//         </PopoverTrigger>
-//         <PopoverContent className="w-72 max-w-sm p-0">
-//           <Command
-//             filter={(value, search) => {
-//               if (value.includes(search)) return 1;
-//               return 0;
-//             }}
-//             // shouldFilter={true}
-//           >
-//             <CommandInput
-//               placeholder={placeholder ?? "Search series..."}
-//               value={query}
-//               onValueChange={(value: string) => setQuery(value)}
-//             />
-//
-//             <ScrollArea>
-//               <div className="max-h-80">
-//                 <CommandGroup>
-//                   <CommandList>
-//                     {options.map((option) => (
-//                       <CommandItem
-//                         key={option.label}
-//                         value={option.label}
-//                         onSelect={() => {
-//                           if (onChange) {
-//                             if (
-//                               mode === "multiple" &&
-//                               Array.isArray(selected)
-//                             ) {
-//                               onChange(
-//                                 selected.includes(option.value)
-//                                   ? selected.filter(
-//                                       (item) => item !== option.value,
-//                                     )
-//                                   : [...selected, option.value],
-//                               )
-//                             } else {
-//                               onChange(option.value)
-//                             }
-//                           }
-//                         }}
-//                       >
-//                         <Check
-//                           className={cn(
-//                             "mr-2 h-4 w-4",
-//                             selected.includes(option.value)
-//                               ? "opacity-100"
-//                               : "opacity-0",
-//                           )}
-//                         />
-//                         {option.label}
-//                       </CommandItem>
-//                     ))}
-//                   </CommandList>
-//                 </CommandGroup>
-//               </div>
-//             </ScrollArea>
-//             <CommandItem
-//               onSelect={() => {
-//                 if (onCreate) {
-//                   onCreate(query)
-//                   setQuery("")
-//                 }
-//               }}
-//               className="flex cursor-pointer items-center justify-center gap-1 pt-0 italic"
-//             >
-//               <p>Create: </p>
-//               <p className="block max-w-48 truncate font-semibold text-primary">
-//                 {query}
-//               </p>
-//             </CommandItem>
-//           </Command>
-//         </PopoverContent>
-//       </Popover>
-//     </div>
-//   )
-// }
 
 const CommandItem = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Item>,
@@ -231,20 +109,6 @@ export function ComboboxWithCreate({
               value={query}
               onValueChange={(value: string) => setQuery(value)}
             />
-            <CommandEmpty
-              onClick={() => {
-                if (onCreate) {
-                  onCreate(query)
-                  setQuery("")
-                }
-              }}
-              className="flex cursor-pointer items-center justify-center gap-1 italic"
-            >
-              <p>Create: </p>
-              <p className="block max-w-48 truncate font-semibold text-primary">
-                {query}
-              </p>
-            </CommandEmpty>
             <ScrollArea>
               <div className="max-h-80">
                 <CommandGroup>
@@ -253,7 +117,7 @@ export function ComboboxWithCreate({
                       <CommandItem
                         key={option.label}
                         value={option.label}
-                        onSelect={(currentValue) => {
+                        onSelect={() => {
                           if (onChange) {
                             if (
                               mode === "multiple" &&
@@ -287,6 +151,22 @@ export function ComboboxWithCreate({
                 </CommandGroup>
               </div>
             </ScrollArea>
+
+            <CommandItem
+              onSelect={() => {
+                if (onCreate) {
+                  onCreate(query)
+                  setQuery("")
+                }
+              }}
+              forceMount
+              className="flex cursor-pointer items-center justify-center gap-1 italic"
+            >
+              <p>Create: </p>
+              <p className="block max-w-48 truncate font-semibold text-primary">
+                {query}
+              </p>
+            </CommandItem>
           </Command>
         </PopoverContent>
       </Popover>
